ece159a5de039a17bfdf0154b44fadf8d650b539
Spop08 <future826@hotmail.com>
2019-12-12 03:42:28 +0100

    confirm email

--- accounts/views.py
+++ accounts/views.py
@@ -13,17 +13,3 @@ class MyAccountView(RetrieveAPIView):
         return self.request.user
 
 
-class ConfirmEmailView():
-    pass
-
-def confirm_login(request):
-	print(request.user.username)
-	return redirect("http://unwravel.com")
-
-class ConfirmAPIView(RetrieveAPIView):
-    permission_classes = (IsAuthenticated,)
-    serializer_class = AccountSerializer
-    print("I am USER")
-    def get(self, *args, **kwargs):
-    	print('USER:',self.request.user)
-	#return redirect("http://unwravel.com")
